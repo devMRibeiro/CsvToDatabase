06f196f0e0a3ae7bf5351f2bcd2dfebce3b64fe7
devMRibeiro <michael.douglas2020@hotmail.com>
2025-05-11 01:54:03 -0300

    feat: adiciona método para criar tabelas

--- src/main/java/com/github/devmribeiro/csvfile/dao/CsvReaderDao.java
+++ src/main/java/com/github/devmribeiro/csvfile/dao/CsvReaderDao.java
@@ -10,6 +10,43 @@ import com.db.utility.impl.ResUtil;
 import com.github.devmribeiro.csvfile.log.Log;
 
 public class CsvReaderDao {
+	private static boolean createTables(Connection conn) {
+
+		PreparedStatement psUF = null;
+		PreparedStatement psMunicipio = null;
+
+		try {
+			psUF = conn.prepareStatement(
+					"create table if not exists unidade_federativa (" +
+					" uf_id serial primary key, " +
+					" uf_cod smallint not null unique, " + 
+					" uf_nome varchar(50) not null, " +
+					" uf_sigla varchar(2) not null, " +
+					" uf_regiao varchar(20) not null)");
+			
+			psMunicipio = conn.prepareStatement(
+					"create table if not exists municipio (" +
+					"  mun_cod integer primary key not null, " +
+					"  mun_nome varchar(100) not null, " +
+					"  mun_uf_cod smallint not null, " +
+					"  constraint fk_unidade_federativa foreign key (mun_uf_cod) references unidade_federativa(uf_cod))");
+
+			if (psUF.executeUpdate() > 0)
+				Log.d(psUF);
+
+			if (psMunicipio.executeUpdate() > 0)
+				Log.d(psMunicipio);
+
+			return true;
+
+		} catch (Exception e) {
+			Log.e("Erro ao criar tabelas", e);
+		} finally {
+			ResUtil.close(psUF, psMunicipio);
+		}
+		return false;
+	}
+	
 	public static boolean insert(Collection<List<String>> data, boolean isMunicipio) {
 
 		Connection conn = null;
@@ -22,7 +59,13 @@ public class CsvReaderDao {
 
 			conn = ResUtil.open();
 
+			if (!createTables(conn))
+				return false;
+
 			ps = conn.prepareStatement(sql);
+
+			int batchSize = 500;
+			int count = 1;
 
 			Iterator<List<String>> itr = data.iterator();
 			if (isMunicipio) {
@@ -35,6 +78,11 @@ public class CsvReaderDao {
 
 					ps.addBatch();
 					Log.d(ps);
+
+					if (count % batchSize == 0)
+	                	ps.executeBatch();
+
+	                count++;
 				}
 			} else {
 				while (itr.hasNext()) {
@@ -46,6 +94,7 @@ public class CsvReaderDao {
 	                ps.setString(4, values.get(3));
 
 	                ps.addBatch();
+
 	                Log.d(ps);
 				}
 			}
