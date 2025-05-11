9e39e2419f38acee02555a79bf8c7324cefad63d
devMRibeiro <michael.douglas2020@hotmail.com>
2025-05-11 01:59:15 -0300

    refactor: ajuste para processar dois tipos de csv. Tanto para UF e Municípios. chore: Adiciona municipios_2024.csv para testes

--- src/main/java/com/github/devmribeiro/csvfile/impl/CsvProcess.java
+++ src/main/java/com/github/devmribeiro/csvfile/impl/CsvProcess.java
@@ -4,40 +4,50 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.github.devmribeiro.csvfile.dao.CsvReaderDao;
 import com.github.devmribeiro.csvfile.log.Log;
 
 public class CsvProcess {
-	public static void processFile() {
+	public static void processMunicipioCsv(String fileName) {
+		processFile(fileName, true);
+	}
+
+	public static void processUnidadeFederativaCsv(String fileName) {
+		processFile(fileName, false);
+	}
+
+	private static void processFile(String fileName, boolean isMunicipio) {
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader("src/main/resources/uf_2024.csv", StandardCharsets.UTF_8));
+			br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8));
 			br.readLine(); // ignora header
 
-			Set<List<String>> set = new HashSet<>();
+			Collection<List<String>> records = (isMunicipio) ? new ArrayList<List<String>>(5573) : new HashSet<List<String>>(27); 
 
 			String line;
-		    while ((line = br.readLine()) != null)
-		        set.add(Arrays.asList(line.split(";")));
+		    while ((line = br.readLine()) != null) 
+		    	records.add(Arrays.asList(line.split(";")));
 
-		    if (CsvReaderDao.insert(set))
-		    	Log.i("Registro inseridos com sucesso");
-		    else
-		    	Log.e("Erro ao inserir");
+			if (CsvReaderDao.insert(records, isMunicipio))
+				Log.i("Registros inseridos com sucesso");
+			else
+				Log.e("Erro ao inserir");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (br != null) try { br.close(); } catch (IOException e) { System.out.println("Erro ao tentar liberar BufferedReader: " + e); }
+			if (br != null) try { br.close(); } catch (IOException e) { Log.e("Erro ao tentar liberar BufferedReader: " + e); }
 		}
 	}
 
 	public static void main(String[] args) {
-		processFile();
+//		processMunicipioCsv("src/main/resources/municipios_2024.csv");
+		processUnidadeFederativaCsv("src/main/resources/uf_2024.csv");
 	}
 }
